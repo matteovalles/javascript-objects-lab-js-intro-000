fff57f49b6b4772b11ce60f287e08b2b769a3ffe
Matteo Valles <matteovalles@gmail.com>
2018-09-09 16:35:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   obj[key] = value
   return obj
 }
+
+function deleteFromObjectByKey(obj, key) {
+  var newObj = obj
+  delete newObj.key
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  delete obj[key]
+  return obj
+}
